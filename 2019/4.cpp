901cf08f4121e01de0e9570a1f2b070cb7b66a7a
Petra Vesela <petra.vesela1993@gmail.com>
2019-12-12 21:30:05 +0100

    first part of day 4

--- 2019/4.cpp
+++ 2019/4.cpp
@@ -1,28 +1,61 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <regex>
 
-void collect_digits(std::vector<short>& digits, unsigned long num) {
-    if (num > 9) {
-        collect_digits(digits, num / 10);
-    }
-    digits.push_back(num % 10);
+void collect_digits(std::vector<short>& digits, long num) {
+	if (num > 9) {
+		collect_digits(digits, num / 10);
+	}
+	digits.push_back(num % 10);
 }
 
-int main(){
-	long start = 123456;
-	long stop = 123457;
-	std::vector<short> digits;
+enum whatToCheck{ justAfter, justBefore, both };
 	
-	for (long int i = start; i < stop; i++){
-		
+int main() {
+	long start = 111122; //236491;
+	long stop = 713787;
+
+	int numOfPasswords = 0;
+	for (long int i = start; i < stop; i++) {
+		std::vector<short> digits;
 		collect_digits(digits, i);
 		short j = 0;
-		while(digits[j] <= digits[++j])
+
+		if ((std::is_sorted(digits.begin(), digits.end()))
+			&& (std::adjacent_find(digits.begin(), digits.end()) != digits.end()))
 		{
-			std::cout << j << std::endl;
+			// Part 2  -- just check neighbouring element being different
+			auto it = std::adjacent_find(digits.begin(), digits.end());
+			
+			int dist = std::distance(digits.begin(), it);
+
+			whatToCheck a = (dist < 4 && dist > 0) ? both : 
+						(dist == 0) ? justAfter :justBefore;
+
+			switch (a)
+			{
+				case both:
+					if (*it == *(std::prev(it)) || *it == *(std::next(it, 2)))
+						continue;
+					break;		
+				case justBefore:
+					if (*it == *(std::prev(it)))
+						continue;
+					break;
+				case justAfter:
+					if (*it == *(std::next(it, 2)))
+						continue;
+					break;
+			};
+			numOfPasswords++;
+
+				
+			// end of Part 2
 		}
-		if (j==5)
-			std::cout << "I'm here";
+		else
+			continue;
 	}
+	std::cout << numOfPasswords;
 	return 0;
 }
