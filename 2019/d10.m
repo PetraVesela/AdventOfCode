9f1ee53812fd534962a96a7f3263295b7fff40d9
Petra Vesela <petra.vesela1993@gmail.com>
2019-12-18 13:27:36 +0100

    Day 10, second part

--- 2019/d10.m
+++ 2019/d10.m
@@ -3,7 +3,7 @@ clearvars; close all; clc
 % NOTE: workaround - I'm working on a fliped map!
 %% retrieve the input:
 file = fopen('inputs/10.txt');
-asteroidMap = zeros(10);
+asteroidMap = zeros(36);
 playGroundSize = size(asteroidMap,1);
 
 for ii = 1:playGroundSize
@@ -21,10 +21,8 @@ asteroidMap(asteroidMap == 35) = 1;
 numAsteroids = sum( sum( asteroidMap));
 aCoords = zeros(2,numAsteroids);
 [aCoords(2,:), aCoords(1,:)] = find(flipud(asteroidMap));
-[emptyCoords(2,:), emptyCoords(1,:)] = find(flipud(~asteroidMap));
-
-allCoords = [aCoords, emptyCoords] - 0.5;
 aCoords = aCoords - 0.5;
+[allCoords(2,:), allCoords(1,:)] = find( ones( size( asteroidMap)));
 canSee = zeros(1,numAsteroids);
 
 % shoot a ray at all other asteroids and get its slope
@@ -35,7 +33,7 @@ for pt1 = aCoords % for every Asteroid
     tryToDetect = aCoords;
     tryToDetect(:,ind) = [];
     
-    x1 = pt1(1); % starting point
+    x1 = pt1(1); % starting point -- set that as origin:
     y1 = pt1(2);
 
     for pt2 = tryToDetect 
@@ -44,27 +42,12 @@ for pt1 = aCoords % for every Asteroid
 %         plot([x1, x2], [y1, y2],'r');
 %         pause(0.2)
 
-        slope = (y1 - y2)/(x1 - x2);
-        if x1<x2
-            slope = slope + 1i; %random workaround
-        else
-            slope = slope -1i;
-        end
-
+        slope = atan2(y2-y1, x2-x1);
         % get thisAsteroid -> otherAsteroids slopes   
-        slopes = (y1 - tryToDetect(2,:))./ (x1 - tryToDetect(1,:));
-        
-        % distinguish +/- direction of the ray
-        for ii = 1: size(slopes,2)
-            if x1 < tryToDetect(1,ii) 
-                slopes(ii) = slopes(ii)+1i;
-            else 
-                slopes(ii) = slopes(ii)-1i;
-            end
-            
-        end     
+        slopes = atan2(tryToDetect(2,:) - y1, tryToDetect(1,:)-x1);
+
         % check     
-        if sum( slopes == slope) % if there is something left visible
+        if sum( slopes == slope) % if there is something in that direction
             canSee(ind) = canSee(ind)+1;
             tryToDetect(:,slopes == slope) = [];
         end        
@@ -76,91 +59,59 @@ end
 [M, I] = max( canSee);
 
 base = aCoords(:,I);
-%% start the vaporization from this point:
+[xBase,yBase] = getCoordinates(base, playGroundSize);
 
-shot = 0;
-
-% It's enough to point at the outermost points of grid:
-% targets = getTargets(playGroundSize);
-% 
-% [~, whereToBegin] = find(targets(1,:) == base(1) & ...
-%                       targets(2,:) == playGroundSize-0.5);
-% targets = circshift(targets, -whereToBegin+1,2);
-
-%currentDirection = Inf;
-plotMap(asteroidMap);
+%% PART 2: start the vaporization from this point:
+ 
+%plotMap(asteroidMap);
 x1 = base(1);
 y1 = base(2);
-slopes = (y1 - aCoords(2,:))./ (x1 - aCoords(1,:));
-[sl, I_slopes] = sort(slopes);
-distances = getDistance(base, aCoords);
 aCoords(:,I) = []; % don't try to shoot yourself
+slopes = atan2(aCoords(2,:) - y1, aCoords(1,:)-x1);
 
+[slopes_sorted, I_slopes] = sort(slopes, 'descend');
+aCoords = aCoords(:,I_slopes);
 
-for target = aCoords
-    plot([base(1), target(1)],[base(2), target(2)], 'g' )
-    x2 = target(1);
-    y2 = target(2);
-    slope = (y1 - y2)/(x1 - x2);
-    if x1<x2
-        slope = slope + 1i;
-    else
-        slope = slope -1i;
+whereToBegin = find(slopes_sorted == pi/2, 1, 'first'); %% todo - might not be there!
+aCoords = [aCoords(:, whereToBegin:end), aCoords(:, 1:whereToBegin-1)];
+slopes_sorted = [slopes_sorted(whereToBegin:end), slopes_sorted(1:whereToBegin-1)];
+ind = 1;
+while ind < length(aCoords)
+
+    ind1 = ind;
+    while ind1 ~= length(slopes_sorted) && slopes_sorted(ind) == slopes_sorted(ind1+1)
+        ind1 = ind1+1;
     end
-
-    % get thisAsteroid -> otherAsteroids slopes   
-    slopes = (y1 - aCoords(2,:))./ (x1 - aCoords(1,:));
-       
-    % distinguish +/- direction of the ray
-    for ii = 1: size(slopes,2)
-        if x1 < aCoords(1,ii) 
-            slopes(ii) = slopes(ii)+1i;
-        else 
-            slopes(ii) = slopes(ii)-1i;
-        end
-    end     
-    if sum( slopes == slope) % if there is something left visible
-        strike = find(slopes == slope);
-        if numel(strike)>1
-           distances = getDistance(base, aCoords(:, strike));
-           [~, closest] = min(distances);
-           strike = strike(closest);
-        end
-        plot(aCoords(1,strike), aCoords(2, strike), 'r*');
-        pause(0.2);
-        aCoords(:,strike) = [];
-        shot = shot+1;
-    end
-
-
-
-%plotMap(asteroidMap)
+    clusters{ind} = aCoords(:, ind:ind1);
+    ind = ind1+1;
 end
 
+clusters = clusters(~cellfun(@isempty, clusters));
+%d = getDistance(base, clusters{3})
+tmp = clusters{200};
+[X,Y] = getCoordinates(tmp, playGroundSize)
 
-[X,Y] = getCoordinates(base(1), base(2), size(asteroidMap, 1));
-%plotMap(asteroidMap)
+
+if size(tmp,2) == 1
+    % sort the same slopes by distance
+end
+
+% for target = aCoords
+%     plot([base(1), target(1)],[base(2), target(2)], 'g' )
+%     pause(0.2)
+% end
+
 
 function dist = getDistance(pt1, pt2)
     dist = sqrt((pt1(1)-pt2(1,:)).^2 + (pt1(2) - pt2(2,:)).^2);
 end
-% 
-% function targets = getTargets(playGroundSize)
-%     cte = playGroundSize - 0.5;
-%     ii = 0.5 : playGroundSize - 0.5;
-%     targets1 = [ii(1:end-1), cte*ones( size( ii)); ...
-%                cte*ones( size( ii(1:end-1))), fliplr(ii)];
-%     targets2 = [fliplr(ii(1:end-2)), 0.5*ones( size(ii(1:end-1))); ...
-%                 0.5*ones( size( ii(1:end-2))), ii(1:end-1)];
-%             
-%     targets = [targets1, targets2];
-% 
-% end
-
-function [X, Y] = getCoordinates(x, y, sz)
+ 
+function [X, Y] = getCoordinates(pt, sz)
+    x = pt(1); y = pt(2);
     X = x - 0.5;
     Y = (sz - 0.5)  - y;
 end
+
 function plotMap(asteroidMap)
     asteroidMap = flipud(asteroidMap);
     playGroundSize = size(asteroidMap,1);
